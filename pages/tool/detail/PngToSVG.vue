46578b320b8fe430db45966759fd005464d3b373
xuxinyuan <204663476@qq.com>
2022-10-28 17:33:16 +0800

    temp

--- pages/tool/detail/PngToSVG.vue
+++ pages/tool/detail/PngToSVG.vue
@@ -4,14 +4,14 @@
             <div class="container px-4 mx-auto">
                 <div class="md:flex md:-mx-4 md:items-center py-8">
                     <div class="md:w-1/2 px-4">
-                        <h1 class="text-2xl text-black">图片转base64</h1>
+                        <h1 class="text-2xl text-black">Png转SVG</h1>
                     </div>
                 </div>
             </div>
         </section>
         <div class="container px-4 mx-auto py-6">
             <div class="form-group">
-                <div class="text-center relative pt-6 pb-6 mb-4 border-2 hover:bg-gray-100 custom-font-14 rounded"
+                <div class="text-center relative py-12 mb-4 border-2 hover:bg-gray-100 custom-font-14 rounded"
                     id="fileInput">
                     <span>拖拽文件到这里或者点击选择文件</span>
                     <input type="file" id="file" accept="image/*"
@@ -21,32 +21,20 @@
             </div>
             <div class="flex flex-wrap">
                 <div class="w-full md:w-6/12 md:pr-2">
-                    <img :src="base64Data.length == 0 ? '/logo-black.png' : base64Data"
-                        class="w-full h-72 object-contain border-2">
+                    <div class="w-full h-72 object-contain border-2" v-html="svgData == '' ? emptyData : svgData"></div>
                 </div>
                 <div class="w-full md:w-6/12 md:pl-2 mt-4 md:mt-0">
-                    <textarea v-model="base64Data"
+                    <textarea v-model="svgData"
                         class="w-full bg-gray-100 outline-none p-2 h-72 text-gray-700 custom-font-14" name="" id=""
                         cols="30" rows="10"></textarea>
                 </div>
             </div>
 
             <div class="flex flex-row flex-wrap justify-around">
-                <button
-                    class="flex my-2 mr-2 py-2 px-4 font-medium tracking-widest text-white uppercase bg-black shadow-lg focus:outline-none hover:bg-gray-900 hover:shadow-none">
-                    Base64转图片
-                </button>
+                <div></div>
                 <div class="flex flex-row flex-wrap">
-                    <button @click="addHeader()"
-                        class="flex m-2 py-2 px-4 font-medium tracking-widest text-white uppercase bg-black shadow-lg focus:outline-none hover:bg-gray-900 hover:shadow-none">
-                        加头
-                    </button>
-                    <button @click="clear()"
-                        class="flex m-2 py-2 px-4 font-medium tracking-widest text-white uppercase bg-black shadow-lg focus:outline-none hover:bg-gray-900 hover:shadow-none">
-                        清空
-                    </button>
                     <button class=" flex m-2 py-2 px-4 font-medium tracking-widest text-white uppercase bg-black shadow-lg
-                focus:outline-none hover:bg-gray-900 hover:shadow-none" @click="toCopy()">
+                focus:outline-none hover:bg-gray-900 hover:shadow-none leading" @click="toCopy()">
                         复制
                     </button>
                 </div>
@@ -57,33 +45,41 @@
     </div>
 </template>
 <script setup lang="ts">
-import { optimize } from 'svgo/dist/svgo.browser'
-// import * as wasm from "~/utils/wasm/pkg/png_to_svg_wasm";
-const wasm = (await import("~/utils/wasm/png-to-svg-wasm"))
+import init, { get_svg } from '~/utils/wasm/png-to-svg-wasm';
+const optimize = (await import('~/utils/svgo.browser.mjs')).optimize;
+const copy = (await import('copy-to-clipboard')).default;
+onMounted(() => {
+    init();
+})
 
 const { $toast } = useNuxtApp();
-const base64Data = ref("");
-
+const emptyData = "<p style=\"line-height:18rem;text-align:center;\">SVG展示区域</p>";
+const svgData = ref("");
 
 async function selectFile(e) {
     if (e.target.files.length > 0) {
-
         const file = e.target.files[0];
         const reader = new FileReader();
         reader.onloadend = async (evt: any) => {
             const u8buffer = new Uint8Array(evt.target.result);
-            let data = await wasm.get_svg(u8buffer);
-            console.log(data);
+            let data = await get_svg(u8buffer);
+            console.log(data)
             const result = optimize(data, {
                 multipass: true,
-                plugins: ['preset-default', 'reusePaths'],
+                plugins: [
+                    'minifyStyles',
+                    'convertShapeToPath',
+                    'convertEllipseToCircle',
+                    'convertPathData',
+                    'mergePaths',
+                ],
                 js2svg: {
                     indent: 2,
                     pretty: true,
                 },
             }).data;
-            document.getElementById("image").innerHTML = result;
-            console.log(result);
+            console.log(result)
+            svgData.value = result
         };
         file && reader.readAsArrayBuffer(file);
 
@@ -97,27 +93,20 @@ definePageMeta({
 })
 
 useHead({
-    title: "图片转Base64",
+    title: "Png转SVG",
     titleTemplate: (title) => `${title} - 工具 - ZNGG在线工具`,
     viewport: 'width=device-width, initial-scale=1, maximum-scale=1',
     charset: 'utf-8',
     meta: [
-        { name: 'Keywords', content: '在线Base64转图片,图片转Base64编码,在线图片转换' },
-        { name: 'description', content: '一个可以将图片和Base64编码相互转换的工具，在图片格式转换时会用到。' }
+        { name: 'Keywords', content: 'Png转SVG,在线将PNG转换成SVG格式,免费PNG转换成SVG,Png to SVG,png conver to svg' },
+        { name: 'description', content: '一个可以在线将PNG图片转换成SVG格式的工具。A tool that can convert PNG images to SVG format online.' }
     ],
 })
 
-function addHeader() {
-    base64Data.value = "data:image/*;base64," + base64Data.value;
-}
 
-function clear() {
-    base64Data.value = "";
-    $toast.success("清除成功");
-}
 
 function toCopy() {
-    navigator.clipboard.writeText(base64Data.value);
+    copy(svgData.value)
     $toast.success("复制成功");
 }
 
---
+++ utils/wasm/png-to-svg-wasm/png_to_svg_wasm.js
@@ -0,0 +1,144 @@
+
+let wasm;
+
+const cachedTextDecoder = new TextDecoder('utf-8', { ignoreBOM: true, fatal: true });
+
+cachedTextDecoder.decode();
+
+let cachedUint8Memory0 = new Uint8Array();
+
+function getUint8Memory0() {
+    if (cachedUint8Memory0.byteLength === 0) {
+        cachedUint8Memory0 = new Uint8Array(wasm.memory.buffer);
+    }
+    return cachedUint8Memory0;
+}
+
+function getStringFromWasm0(ptr, len) {
+    return cachedTextDecoder.decode(getUint8Memory0().subarray(ptr, ptr + len));
+}
+
+let WASM_VECTOR_LEN = 0;
+
+function passArray8ToWasm0(arg, malloc) {
+    const ptr = malloc(arg.length * 1);
+    getUint8Memory0().set(arg, ptr / 1);
+    WASM_VECTOR_LEN = arg.length;
+    return ptr;
+}
+
+let cachedInt32Memory0 = new Int32Array();
+
+function getInt32Memory0() {
+    if (cachedInt32Memory0.byteLength === 0) {
+        cachedInt32Memory0 = new Int32Array(wasm.memory.buffer);
+    }
+    return cachedInt32Memory0;
+}
+/**
+* @param {Uint8Array} raw
+* @returns {string}
+*/
+export function get_svg(raw) {
+    try {
+        const retptr = wasm.__wbindgen_add_to_stack_pointer(-16);
+        const ptr0 = passArray8ToWasm0(raw, wasm.__wbindgen_malloc);
+        const len0 = WASM_VECTOR_LEN;
+        wasm.get_svg(retptr, ptr0, len0);
+        var r0 = getInt32Memory0()[retptr / 4 + 0];
+        var r1 = getInt32Memory0()[retptr / 4 + 1];
+        return getStringFromWasm0(r0, r1);
+    } finally {
+        wasm.__wbindgen_add_to_stack_pointer(16);
+        wasm.__wbindgen_free(r0, r1);
+    }
+}
+
+async function load(module, imports) {
+    if (typeof Response === 'function' && module instanceof Response) {
+        if (typeof WebAssembly.instantiateStreaming === 'function') {
+            try {
+                return await WebAssembly.instantiateStreaming(module, imports);
+
+            } catch (e) {
+                if (module.headers.get('Content-Type') != 'application/wasm') {
+                    console.warn("`WebAssembly.instantiateStreaming` failed because your server does not serve wasm with `application/wasm` MIME type. Falling back to `WebAssembly.instantiate` which is slower. Original error:\n", e);
+
+                } else {
+                    throw e;
+                }
+            }
+        }
+
+        const bytes = await module.arrayBuffer();
+        return await WebAssembly.instantiate(bytes, imports);
+
+    } else {
+        const instance = await WebAssembly.instantiate(module, imports);
+
+        if (instance instanceof WebAssembly.Instance) {
+            return { instance, module };
+
+        } else {
+            return instance;
+        }
+    }
+}
+
+function getImports() {
+    const imports = {};
+    imports.wbg = {};
+    imports.wbg.__wbindgen_throw = function(arg0, arg1) {
+        throw new Error(getStringFromWasm0(arg0, arg1));
+    };
+
+    return imports;
+}
+
+function initMemory(imports, maybe_memory) {
+
+}
+
+function finalizeInit(instance, module) {
+    wasm = instance.exports;
+    init.__wbindgen_wasm_module = module;
+    cachedInt32Memory0 = new Int32Array();
+    cachedUint8Memory0 = new Uint8Array();
+
+
+    return wasm;
+}
+
+function initSync(module) {
+    const imports = getImports();
+
+    initMemory(imports);
+
+    if (!(module instanceof WebAssembly.Module)) {
+        module = new WebAssembly.Module(module);
+    }
+
+    const instance = new WebAssembly.Instance(module, imports);
+
+    return finalizeInit(instance, module);
+}
+
+async function init(input) {
+    if (typeof input === 'undefined') {
+        input = new URL('png_to_svg_wasm_bg.wasm', import.meta.url);
+    }
+    const imports = getImports();
+
+    if (typeof input === 'string' || (typeof Request === 'function' && input instanceof Request) || (typeof URL === 'function' && input instanceof URL)) {
+        input = fetch(input);
+    }
+
+    initMemory(imports);
+
+    const { instance, module } = await load(await input, imports);
+
+    return finalizeInit(instance, module);
+}
+
+export { initSync }
+export default init;
