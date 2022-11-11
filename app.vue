d648ea4c24810caa5b58200c387817d3521303f5
xuxinyuan <204663476@qq.com>
2022-11-11 17:38:54 +0800

    ad

--- app.vue
+++ app.vue
@@ -4,4 +4,12 @@
 <script setup lang="ts">
 import '~/assets/fonts/iconfont.css';
 
+useHead({
+    script: [
+        { async: "async", src: 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6667301035180632', crossorigin: "anonymous" }
+    ]
+})
+
+
+
 </script>
